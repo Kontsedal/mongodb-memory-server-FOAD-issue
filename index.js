71d554dfce5fdf1327c50a15e863da687e3679ac
Kontsedal <kontsedalbohdan@gmail.com>
2022-08-29 14:12:58 +0300

    add date

--- index.js
+++ index.js
@@ -1,37 +1,50 @@
-const { MongoClient, Decimal128} = require("mongodb");
+const { MongoClient, Decimal128 } = require("mongodb");
 const { MongoMemoryServer } = require("mongodb-memory-server");
+const moment = require("moment");
 const assert = require("assert");
 
 async function getCollection() {
   let connectionUrl;
   switch (process.env.DB) {
-    case 'mongo':
-      connectionUrl = "mongodb://localhost:27017/find-issue"
+    case "mongo":
+      connectionUrl = "mongodb://localhost:27017/find-issue";
       break;
-    case 'fake-mongo':
+    case "fake-mongo":
       const mongod = await MongoMemoryServer.create();
-      connectionUrl = mongod.getUri()
-      break
+      connectionUrl = mongod.getUri();
+      break;
     default:
-      throw new Error("ENV DB is not provided")
+      throw new Error("ENV DB is not provided");
   }
   const client = new MongoClient(connectionUrl);
-  await client.connect()
-  const collection = client.db().collection('limit');
+  await client.connect();
+  const collection = client.db().collection("limit");
   await collection.createIndex(
     {
-      "limitName": 1
+      limitName: 1,
+      date: 1,
     },
     { unique: true, background: false }
-  )
-  return collection
+  );
+  return collection;
 }
 
-async function incrementUsedLimit({ collection, limitName, amount, totalAmount }) {
+async function incrementUsedLimit({
+  collection,
+  limitName,
+  amount,
+  totalAmount,
+  date,
+}) {
   return collection.findOneAndUpdate(
-    { limitName, totalAmount, usedAmount: { $lt:  Decimal128.fromString(totalAmount.toString()) } },
     {
-      $inc: { usedAmount:  Decimal128.fromString(amount.toString()) },
+      limitName,
+      date,
+      totalAmount,
+      usedAmount: { $lt: Decimal128.fromString(totalAmount.toString()) },
+    },
+    {
+      $inc: { usedAmount: Decimal128.fromString(amount.toString()) },
     },
     {
       upsert: true,
@@ -43,15 +56,27 @@ async function incrementUsedLimit({ collection, limitName, amount, totalAmount }
 async function test() {
   const collection = await getCollection();
   const limitName = `limit_${Date.now()}`;
+  const date = moment(new Date()).utc().startOf("day").toDate();
   const totalAmount = 100;
   const result = await Promise.allSettled([
-    incrementUsedLimit({ collection, limitName, amount: 99, totalAmount }),
-    incrementUsedLimit({ collection, limitName, amount: 1, totalAmount }),
-    incrementUsedLimit({ collection, limitName, amount: 1, totalAmount }),
+    incrementUsedLimit({
+      collection,
+      date,
+      limitName,
+      amount: 99,
+      totalAmount,
+    }),
+    incrementUsedLimit({ collection, date, limitName, amount: 1, totalAmount }),
+    incrementUsedLimit({ collection, date, limitName, amount: 1, totalAmount }),
   ]);
-  const successfulOperations = result.filter(item => item.status === "fulfilled");
-  assert.ok(successfulOperations.length === 2, "There should be no more that 2 successful operations");
-  console.log("Success")
+  const successfulOperations = result.filter(
+    (item) => item.status === "fulfilled"
+  );
+  assert.ok(
+    successfulOperations.length === 2,
+    "There should be no more that 2 successful operations"
+  );
+  console.log("Success");
 }
 
 test().catch(console.error);
